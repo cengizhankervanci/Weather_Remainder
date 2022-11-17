e8c35b8eee9943e8daf5e1e41c3fa01e4331d441
cengizhankervanci <cengizhankervanci@gmail.com>
2022-11-17 15:59:07 +0300

    Dev: Design Ready

--- Weather Remainder/Weather Remainder/ContentView.swift
+++ Weather Remainder/Weather Remainder/ContentView.swift
@@ -7,13 +7,90 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+let Day = Gradient(stops: [
+    .init(color: Color.blue.opacity(0.6), location: 0.5),
+    .init(color: Color.white, location: 1)]
+)
+
+let Night = Gradient(stops: [
+    .init(color: Color.gray.opacity(1), location: 0.5),
+    .init(color: Color.black, location: 1)
+])
+
+struct ContentView: View
+{
+    var body: some View
+    {
+        ZStack
+        {
+            
+            Rectangle()
+                .fill(LinearGradient(gradient: Day, startPoint:.top, endPoint:.bottomTrailing))
+                .frame(width:UIScreen.main.bounds.width,height: UIScreen.main.bounds.height * 1.2)
+            
+            VStack
+            {
+                Group
+                {
+                    Text("Kartal , İSTANBUL")
+                        .font(.system(size: 40))
+                        .foregroundColor(.white)
+                        .fontWeight(.bold)
+                    
+                    Text("Güneşli")
+                        .font(.title3)
+                        .foregroundColor(.white)
+                        .fontWeight(.bold)
+                }
+                
+                Group
+                {
+                    Image("sun")
+                        .resizable()
+                        .frame(width: 260,height: 260)
+                    
+                    Text("24°")
+                        .font(.system(size: 60))
+                        .foregroundColor(.white)
+                        .shadow(color:.white,radius: 1)
+                    
+                    Text("Hava güneşli ve son derece açık gözüküyor. Günün keyfini çıkarın.")
+                        .foregroundColor(.white)
+                        .fontWeight(.bold)
+                }
+                
+                
+                Group
+                {
+                    ZStack
+                    {
+                        RoundedRectangle(cornerRadius: 35)
+                            .frame(width: UIScreen.main.bounds.width,height: UIScreen.main.bounds.height / 3.5)
+                            .offset(y:90)
+                            .foregroundColor(.white.opacity(0.2))
+                            .shadow(color:.white,radius: 10)
+                        
+                        HStack
+                        {
+                            Text("Min Temp : 22°")
+                            Spacer()
+                            Text("Max Temp : 25°")
+                        }.padding()
+                            .offset(y:20)
+                        
+                            HStack
+                            {
+                                Text("Wind Speed : 1 m/s")
+                                Spacer()
+                                Text("Humidity : %10")
+                            }.padding()
+                            .offset(y:75)
+                        
+                    }
+                }
+                .offset(y:30)
+                
+            }
         }
         .padding()
     }
