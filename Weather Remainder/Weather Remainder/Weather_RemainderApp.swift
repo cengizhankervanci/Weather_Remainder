1a846513dd6548d5950a76905a3eccc36244253e
cengizhankervanci <cengizhankervanci@gmail.com>
2022-11-17 12:09:20 +0300

    Dev: Project Creating

---
+++ Weather Remainder/Weather Remainder/Weather_RemainderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_RemainderApp.swift
+//  Weather Remainder
+//
+//  Created by Cengizhan Kervancı on 17.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct Weather_RemainderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
